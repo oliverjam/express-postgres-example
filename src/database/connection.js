90d645fa34acf3c132dbbc758cf7d72a453e2c98
oliverjam <oliverjamesphillips@gmail.com>
2021-10-04 12:16:30 +0100

    Simplify scripts and update dependencies

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,15 +1,11 @@
 const pg = require("pg");
-const dotenv = require("dotenv");
-dotenv.config();
 
-const DB_URL = process.env.DATABASE_URL;
-
-if (!DB_URL) throw new Error("DATABASE_URL env var not found");
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL environment variable is not set");
+}
 
 const db = new pg.Pool({
-  connectionString: DB_URL,
+  connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = db;
-
-db.query("SELECT * FROM users").then(console.log);
